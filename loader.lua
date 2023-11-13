1c8f1f18705284d3c9733a8cc472298251047c44
goslingscripts <150567568+goslingscripts@users.noreply.github.com>
2023-11-13 21:49:52 +0800

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,6 @@
+-- Gosling Script Loader
+
+-- Jujutsu Chronicles
+if game.PlaceId == 11200494415 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/goslingscripts/public/main/JJC.lua"))()
+end
